4a180f6a46ecf7fb1bae2b3563c1a9d97555baf6
iryudragon01 <iryudragon01@gmail.com>
2021-05-08 19:19:32 +0700

    first commit

---
+++ app/src/main/java/com/wahkor/todolist/DashboardActivity.kt
@@ -0,0 +1,41 @@
+package com.wahkor.todolist
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+        val signOutButton = findViewById<Button>(R.id.sign_out_button)
+        signOutButton.setOnClickListener { signOut() }
+    }
+    private fun signOut() {
+
+        Firebase.auth.signOut()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        // Google sign out
+        mGoogleSignInClient.signOut().addOnCompleteListener(
+            this
+        ) {
+            val mainIntent = Intent(this,MainActivity::class.java)
+            startActivity(mainIntent) }
+
+    }
+}
